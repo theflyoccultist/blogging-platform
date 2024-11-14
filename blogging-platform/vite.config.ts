4fa83b8cc8bb6cfdf5be4a7cfe0f348193d36962
theflyoccultist <y.delahaije@gmail.com>
2024-11-14 13:05:32 +0100

    blog platform still v0.1

--- blogging-platform/vite.config.ts
+++ blogging-platform/vite.config.ts
@@ -5,6 +5,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/blogging-platform' 
-  // to uncomment during deployment
 })
